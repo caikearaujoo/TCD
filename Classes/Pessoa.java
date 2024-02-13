392cec05d813150dc799eccef6db001d689757a4
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-02-13 19:10:23 -0300

    Create Pessoa.java

---
+++ Classes/Pessoa.java
@@ -0,0 +1,94 @@
+package udidecola;
+
+public class Pessoa {
+	private String nome;
+	private String endLogradouro;
+	private int endNumero;
+	private String endBairro;
+	private String endCidade;
+	private String endUf;
+	private String endCep;
+	private String cpf;
+	private int diaNasc;
+	private int mesNasc;
+	private int anoNasc;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getEndLogradouro() {
+		return endLogradouro;
+	}
+	public void setEndLogradouro(String endLogradouro) {
+		this.endLogradouro = endLogradouro;
+	}
+	
+	public int getEndNumero() {
+		return endNumero;
+	}
+	public void setEndNumero(int endNumero) {
+		this.endNumero = endNumero;
+	}
+	
+	public String getEndBairro() {
+		return endBairro;
+	}
+	public void setEndBairro(String endBairro) {
+		this.endBairro = endBairro;
+	}
+	
+	public String getEndCidade() {
+		return endCidade;
+	}
+	public void setEndCidade(String endCidade) {
+		this.endCidade = endCidade;
+	}
+	
+	public String getEndUf() {
+		return endUf;
+	}
+	public void setEndUf(String endUf) {
+		this.endUf = endUf;
+	}
+	
+	public String getEndCep() {
+		return endCep;
+	}
+	public void setEndCep(String endCep) {
+		this.endCep = endCep;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public int getDiaNasc() {
+		return diaNasc;
+	}
+	public void setDiaNasc(int diaNasc) {
+		this.diaNasc = diaNasc;
+	}
+	
+	public int getMesNasc() {
+		return mesNasc;
+	}
+	public void setMesNasc(int mesNasc) {
+		this.mesNasc = mesNasc;
+	}
+	
+	public int getAnoNasc() {
+		return anoNasc;
+	}
+	public void setAnoNasc(int anoNasc) {
+		this.anoNasc = anoNasc;
+	}
+	
+	// métodos
+}
