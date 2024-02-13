70b1b9ce213ee6f5a43cf83bb59e0e8f0f5d1d0f
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-02-13 19:10:52 -0300

    Create Cliente.java

---
+++ Classes/Cliente.java
@@ -0,0 +1,46 @@
+package udidecola;
+
+public class Cliente extends Pessoa {
+	private int diaCad;
+	private int mesCad;
+	private int anoCad;
+	private String email;
+	private int numCompras;
+	
+	public int getDiaCad() {
+		return diaCad;
+	}
+	public void setDiaCad(int diaCad) {
+		this.diaCad = diaCad;
+	}
+	
+	public int getMesCad() {
+		return mesCad;
+	}
+	public void setMesCad(int mesCad) {
+		this.mesCad = mesCad;
+	}
+	
+	public int getAnoCad() {
+		return anoCad;
+	}
+	public void setAnoCad(int anoCad) {
+		this.anoCad = anoCad;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public int getNumCompras() {
+		return numCompras;
+	}
+	public void setNumCompras(int numCompras) {
+		this.numCompras = numCompras;
+	}
+	
+	// métodos	
+}
