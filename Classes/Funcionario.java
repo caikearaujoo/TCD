5127f0fd7a98ac5692a9bf27a984205d0d7596b2
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-02-13 19:11:41 -0300

    Create Funcionario.java

---
+++ Classes/Funcionario.java
@@ -0,0 +1,14 @@
+package udidecola;
+
+public class FuncEspecial extends Funcionario{
+	private double bonusSal;
+
+	public double getBonusSal() {
+		return bonusSal;
+	}
+	public void setBonusSal(double bonusSal) {
+		this.bonusSal = bonusSal;
+	}
+	
+	// métodos
+}
