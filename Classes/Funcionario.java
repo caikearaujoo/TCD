926e739679d798cbaccde9d164c223e28fab89ec
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-02-13 19:12:25 -0300

    Create Funcionario.java

---
+++ Classes/Funcionario.java
@@ -0,0 +1,30 @@
+package udidecola;
+
+public class Funcionario extends Pessoa{
+	private String carteiraTrab;
+	private double salarioFixo;
+	private int hoteisCad;
+	
+	public String getCarteiraTrab() {
+		return carteiraTrab;
+	}
+	public void setCarteiraTrab(String carteiraTrab) {
+		this.carteiraTrab = carteiraTrab;
+	}
+	
+	public double getSalarioFixo() {
+		return salarioFixo;
+	}
+	public void setSalarioFixo(double salarioFixo) {
+		this.salarioFixo = salarioFixo;
+	}
+	
+	public int getHoteisCad() {
+		return hoteisCad;
+	}
+	public void setHoteisCad(int hoteisCad) {
+		this.hoteisCad = hoteisCad;
+	}
+	
+	// métodos
+}
