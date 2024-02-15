c2f6f41f38ea5b771bbe74fa41f1c1ad4046ead9
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2024-02-15 18:56:55 -0300

    Update FuncEspecial.java

--- Classes/FuncEspecial.java
+++ Classes/FuncEspecial.java
@@ -1,8 +1,9 @@
-package udidecola;
+package tcd;
 
 public class FuncEspecial extends Funcionario{
 	private double bonusSal;
-
+	private int numHotelRegistrado = 0; //Ou voce deixa 0 aqui, ou no construtor
+	
 	public double getBonusSal() {
 		return bonusSal;
 	}
@@ -10,5 +11,10 @@ public class FuncEspecial extends Funcionario{
 		this.bonusSal = bonusSal;
 	}
 	
-	// métodos
+	public Hotel registraHotel(String CNPJ, String nome, String nomeDivulg, int diaCria, int mesCria, int anoCria, String endLogradouro, int endNumero, String endBairro, String endCidade, String endUf, String endCep, int numEstrelas, int accPet, int numQuartos, String msgDivulg, String desc, String cidade)
+	{
+		Hotel h = new Hotel(CNPJ, nome, nomeDivulg, diaCria, mesCria, anoCria, endLogradouro, endNumero, endBairro, endCidade, endUf, endCep, numEstrelas, accPet, numQuartos, msgDivulg, desc, cidade);
+		numHotelRegistrado++;
+		return h;
+	}
 }
