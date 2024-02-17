dd14a42dda17b6224b0f9e4fb9964c2279fc9fb5
Matheus Saliba <127700224+SalibaM@users.noreply.github.com>
2024-02-16 21:03:46 -0300

    Update CompanhiaAerea.java

--- Classes/CompanhiaAerea.java
+++ Classes/CompanhiaAerea.java
@@ -1,14 +1,13 @@
-import java.util.Date;
 
 class CompanhiaAerea extends Empresa{
+	private final static double taxaUdi = 0.05;
 	
-	public CompanhiaAerea(String CNPJ, String nome, String nomeDivulg, Date dataCria, String endLogradouro,
-			int endNumero, String endBairro, String endCidade, String endUf, String endCep) {
-		super(CNPJ, nome, nomeDivulg, dataCria, endLogradouro, endNumero, endBairro, endCidade, endUf, endCep);
+	public CompanhiaAerea(String CNPJ, String nome, String nomeDivulg, String endLogradouro, int diaCria, int mesCria,
+			int anoCria, int endNumero, String endBairro, String endCidade, String endUf,
+			String endCep) {
+		super(CNPJ, nome, nomeDivulg, endLogradouro, diaCria, mesCria, anoCria, endNumero, endBairro, endCidade,
+				endUf, endCep);
 	}
-
-	final static double taxaUdi = 0.05;
-	
 	
 	public double getTaxaUdi() {
 		return taxaUdi;
