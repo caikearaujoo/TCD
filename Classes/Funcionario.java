ad6dccccfc10789c24ca9e4a873991859b3b0bd4
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-02-16 21:31:28 -0300

    Update Funcionario.java

--- Classes/Funcionario.java
+++ Classes/Funcionario.java
@@ -2,29 +2,34 @@ package udidecola;
 
 public class Funcionario extends Pessoa{
 	private String carteiraTrab;
-	private double salarioFixo;
+	private static double salarioFixo = 3000.0;
 	private int hoteisCad;
+	
+	public Funcionario(String nome, String endLogradouro, int endNumero, String endBairro, String endCidade, String endUf, String endCep, String cpf, int diaNasc, int mesNasc, int anoNasc, String carteiraTrab, int hoteisCad) {
+		super(nome, endLogradouro, endNumero, endBairro, endCidade, endUf, endCep, cpf, diaNasc, mesNasc, anoNasc);
+		setCarteiraTrab(carteiraTrab);
+		setHoteisCad(hoteisCad);
+	}
 	
 	public String getCarteiraTrab() {
 		return carteiraTrab;
 	}
+	
 	public void setCarteiraTrab(String carteiraTrab) {
-		this.carteiraTrab = carteiraTrab;
+		if (carteiraTrab.length() == 9) this.carteiraTrab = carteiraTrab;
+		else this.carteiraTrab = null;
 	}
 	
 	public double getSalarioFixo() {
 		return salarioFixo;
 	}
-	public void setSalarioFixo(double salarioFixo) {
-		this.salarioFixo = salarioFixo;
-	}
-	
+
 	public int getHoteisCad() {
 		return hoteisCad;
 	}
+
 	public void setHoteisCad(int hoteisCad) {
-		this.hoteisCad = hoteisCad;
+		if (hoteisCad >= 0) this.hoteisCad = hoteisCad;
+		else this.hoteisCad = -1;
 	}
-	
-	// métodos
 }
