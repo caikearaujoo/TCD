8f3d8493acc8557019ef038f876e6b502c33b151
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-02-16 21:07:01 -0300

    Update Pessoa.java

--- Classes/Pessoa.java
+++ Classes/Pessoa.java
@@ -1,5 +1,8 @@
 package udidecola;
 
+import meuPacote.VerificaCPF;
+import java.time.LocalDate;
+
 public class Pessoa {
 	private String nome;
 	private String endLogradouro;
@@ -9,13 +12,28 @@ public class Pessoa {
 	private String endUf;
 	private String endCep;
 	private String cpf;
-	private int diaNasc;
-	private int mesNasc;
-	private int anoNasc;
+	private LocalDate dataNasc;
+	
+	public Pessoa(String nome, String endLogradouro, int endNumero, String endBairro, String endCidade, String endUf, String endCep, String cpf, int diaNasc, int mesNasc, int anoNasc) {
+		setNome(nome);
+		setEndLogradouro(endLogradouro);
+		setEndNumero(endNumero);
+		setEndBairro(endBairro);
+		setEndCidade(endCidade);
+		setEndUf(endUf);
+		setEndCep(endCep);
+		setCpf(cpf);
+		setDataNasc(diaNasc, mesNasc, anoNasc);
+	}
+	
+	public Pessoa() {
+		
+	}
 	
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
@@ -23,6 +41,7 @@ public class Pessoa {
 	public String getEndLogradouro() {
 		return endLogradouro;
 	}
+	
 	public void setEndLogradouro(String endLogradouro) {
 		this.endLogradouro = endLogradouro;
 	}
@@ -30,6 +49,7 @@ public class Pessoa {
 	public int getEndNumero() {
 		return endNumero;
 	}
+	
 	public void setEndNumero(int endNumero) {
 		this.endNumero = endNumero;
 	}
@@ -37,6 +57,7 @@ public class Pessoa {
 	public String getEndBairro() {
 		return endBairro;
 	}
+	
 	public void setEndBairro(String endBairro) {
 		this.endBairro = endBairro;
 	}
@@ -44,6 +65,7 @@ public class Pessoa {
 	public String getEndCidade() {
 		return endCidade;
 	}
+	
 	public void setEndCidade(String endCidade) {
 		this.endCidade = endCidade;
 	}
@@ -51,44 +73,83 @@ public class Pessoa {
 	public String getEndUf() {
 		return endUf;
 	}
+	
 	public void setEndUf(String endUf) {
-		this.endUf = endUf;
+		if (endUf.length() == 2) this.endUf = endUf;
+		else endUf = null;
 	}
 	
 	public String getEndCep() {
 		return endCep;
 	}
+	
 	public void setEndCep(String endCep) {
-		this.endCep = endCep;
+		if (endCep.length() == 8) this.endCep = endCep;
+		else endCep = null;
 	}
 	
 	public String getCpf() {
 		return cpf;
 	}
+	
 	public void setCpf(String cpf) {
-		this.cpf = cpf;
+		if (VerificaCPF.isCPF(cpf)) this.cpf = cpf;
+		else this.cpf = null;
 	}
 	
-	public int getDiaNasc() {
-		return diaNasc;
-	}
-	public void setDiaNasc(int diaNasc) {
-		this.diaNasc = diaNasc;
+	public LocalDate getDataNasc() {
+		return dataNasc;
 	}
 	
-	public int getMesNasc() {
-		return mesNasc;
-	}
-	public void setMesNasc(int mesNasc) {
-		this.mesNasc = mesNasc;
-	}
-	
-	public int getAnoNasc() {
-		return anoNasc;
-	}
-	public void setAnoNasc(int anoNasc) {
-		this.anoNasc = anoNasc;
-	}
-	
-	// métodos
+    public void setDataNasc(int dia, int mes, int ano) {
+        if (ano < 0) {
+        	dia = -1;
+            mes = -1;
+            ano = -1;
+        }
+        
+        if ((mes < 0) && (mes > 13)) {
+            dia = -1;
+            mes = -1;
+            ano = -1;
+        }
+        
+        if (dia < 0) {
+        	dia = -1;
+        	mes = -1;
+        	ano = -1;
+        }
+        
+        if ((mes == 2) && (ano % 4 == 0) && ((ano % 100 != 0) || (ano % 400 == 0))) {
+            if ((dia < 0) && (dia > 29)) {
+                dia = -1;
+                mes = -1;
+                ano = -1;
+            }
+        } else if ((mes == 2) && (dia < 0) && (dia > 28)) {
+        	dia = -1;
+            mes = -1;
+            ano = -1;
+        }
+        
+        if ((mes == 1) || (mes == 3) || (mes == 5) || (mes == 7) || (mes == 8) || (mes == 10) || (mes == 12)) {
+        	if ((dia < 0) && (dia > 31)) {
+        		dia = -1;
+        		mes = -1;
+        		ano = -1;
+        	}
+        }
+        
+        if ((mes == 4) || (mes == 6) || (mes == 9) || (mes == 11)) {
+        	if ((dia < 0) && (dia > 30)) {
+        		dia = -1;
+        		mes = -1;
+        		ano = -1;
+        	}
+        }
+        
+        if ((dia != -1) && (mes != -1) && (ano != -1)) {
+        	this.dataNasc = LocalDate.of(ano, mes, dia);
+        }        
+    }
 }
