9f8de12ac1369a99be63b127849bbca12f62679f
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2024-02-16 22:00:48 -0300

    Create verificaPlaca.java

---
+++ Pacotes/verificaPlaca.java
@@ -0,0 +1,17 @@
+package meuPacote;
+
+public class verificaPlaca {
+	public static boolean validaPlaca(String placa){
+		   boolean valido = true;
+		   if(placa.length() != 7){
+		      valido = false;
+		   }
+		   if(!placa.substring(0, 3).matches("[A-Z]*")){
+		      valido = false;
+		   }
+		   if(!placa.substring(3).matches("[0-9]*")){
+		      valido = false;
+		   }
+		   return valido;
+		}
+}
