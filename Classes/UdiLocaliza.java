6b6b0f12782107d5935d2f00b9daa50cc182e154
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2024-02-18 20:42:05 -0300

    Update UdiLocaliza.java

--- Classes/UdiLocaliza.java
+++ Classes/UdiLocaliza.java
@@ -1,17 +1,26 @@
-import java.time.LocalTime;
-import java.util.Date;
+package tcd;
+
 import java.util.ArrayList;
 
 class UdiLocaliza extends Empresa {
+	
+	private ArrayList<Carros> carros;
+	private ArrayList<Funcionario> funcionarios;
+	
     public UdiLocaliza(String CNPJ, String nome, String nomeDivulg, String endLogradouro, int diaCria, int mesCria,
 			int anoCria, int endNumero, String endBairro, String endCidade, String endUf, String endCep) {
 		super(CNPJ, nome, nomeDivulg, endLogradouro, diaCria, mesCria, anoCria, endNumero, endBairro, endCidade, endUf, endCep);
 		carros = new ArrayList<>();
+		funcionarios = new ArrayList<>();
 	}
 
-	private ArrayList<Carros> carros;
-    
-    
+	public void adicionarFuncionario(Funcionario funcionario) {
+        funcionarios.add(funcionario);
+    }
+
+    public void removerFuncionario(Funcionario funcionario) {
+        funcionarios.remove(funcionario);
+    }
     
     public void adicionarCarro(Carros carro) {
         carros.add(carro);
