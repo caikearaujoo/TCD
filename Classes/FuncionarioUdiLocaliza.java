1cd93678e63196567b78f3eb795ac4ce9ca9c3f1
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2024-02-18 20:42:37 -0300

    Create FuncionarioUdiLocaliza.java

---
+++ Classes/FuncionarioUdiLocaliza.java
@@ -0,0 +1,25 @@
+package tcd;
+
+class FuncionarioUdiLocaliza extends Funcionario 
+{
+	public FuncionarioUdiLocaliza(String nome, String endLogradouro, int endNumero, String endBairro, String endCidade,
+			String endUf, String endCep, String cpf, int diaNasc, int mesNasc, int anoNasc, String carteiraTrab,
+			int hoteisCad) {
+		super(nome, endLogradouro, endNumero, endBairro, endCidade, endUf, endCep, cpf, diaNasc, mesNasc, anoNasc, carteiraTrab,
+				hoteisCad);
+	}
+	
+	public void adicionarCarro(UdiLocaliza locadora, Carros carro) {
+        locadora.adicionarCarro(carro);
+    }
+	
+	public void removerCarro(UdiLocaliza locadora, Carros carro) {
+        locadora.removerCarro(carro);
+    }
+	
+	public void vendaCarro(Carros carro, int diaDevolucao, int mesDevolucao, int anoDevolucao, int diaRetirada, int mesRetirada, int anoRetirada, int horaRetirada, int minRetirada, int segRetirada, int horaDevolucao, int minDevolucao, int segDevolucao, Pessoa clienteLocador)
+	{
+		carro.vendaCarro(carro, diaDevolucao, mesDevolucao, anoDevolucao, diaRetirada, mesRetirada, anoRetirada, horaRetirada, minRetirada, segRetirada, horaDevolucao, minDevolucao, segDevolucao, clienteLocador);
+	}
+
+}
