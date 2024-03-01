35b2023cd05975a2810e784dc3ab1337069ee0a5
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2024-03-01 19:31:25 -0300

    Update Hotel.java

--- TCD atualizado/Udi-Decola/Hotel.java
+++ TCD atualizado/Udi-Decola/Hotel.java
@@ -224,7 +224,7 @@ class Hotel extends Empresa
 	}
 	
 	public Hotel(String CNPJ, String nome, String nomeDivulg, String endLogradouro, int diaCria, int mesCria,
-			int anoCria, int endNumero, String endBairro, String endCidade, String endUf, String endCep,int numEstrelas,int accPet,int numQuartos,String msgDivulg,String desc,String cidade,int hCheckin,int minCheckin,int segCheckin,int hCheckout,int minCheckout,int segCheckout) {
+			int anoCria, int endNumero, String endBairro, String endCidade, String endUf, String endCep,int numEstrelas,boolean accPet,int numQuartos,String msgDivulg,String desc,String cidade,int hCheckin,int minCheckin,int segCheckin,int hCheckout,int minCheckout,int segCheckout) {
 		
 		super(CNPJ, nome, nomeDivulg, endLogradouro, diaCria, mesCria, anoCria, endNumero, endBairro, endCidade, endUf, endCep);
 		
