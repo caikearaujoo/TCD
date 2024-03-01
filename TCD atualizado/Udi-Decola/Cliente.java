b601b82a4bb7b6ebafd9f1ca9fdd4595008636d7
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-03-01 18:27:01 -0300

    Update Cliente.java

--- TCD atualizado/Udi-Decola/Cliente.java
+++ TCD atualizado/Udi-Decola/Cliente.java
@@ -33,58 +33,58 @@ public class Cliente extends Pessoa {
 	public LocalDate getDataCad() {
 		return dataCad;
 	}
-
+	
 	public void setDataCad(int dia, int mes, int ano) {
-        if (ano < 0) {
-        	dia = -1;
-            mes = -1;
-            ano = -1;
-        }
-        
-        if ((mes < 0) && (mes > 13)) {
-            dia = -1;
-            mes = -1;
-            ano = -1;
-        }
-        
-        if (dia < 0) {
-        	dia = -1;
-        	mes = -1;
-        	ano = -1;
-        }
-        
-        if ((mes == 2) && (ano % 4 == 0) && ((ano % 100 != 0) || (ano % 400 == 0))) {
-            if ((dia < 0) && (dia > 29)) {
-                dia = -1;
-                mes = -1;
-                ano = -1;
-            }
-        } else if ((mes == 2) && (dia < 0) && (dia > 28)) {
-        	dia = -1;
-            mes = -1;
-            ano = -1;
-        }
-        
-        if ((mes == 1) || (mes == 3) || (mes == 5) || (mes == 7) || (mes == 8) || (mes == 10) || (mes == 12)) {
-        	if ((dia < 0) && (dia > 31)) {
-        		dia = -1;
-        		mes = -1;
-        		ano = -1;
-        	}
-        }
-        
-        if ((mes == 4) || (mes == 6) || (mes == 9) || (mes == 11)) {
-        	if ((dia < 0) && (dia > 30)) {
-        		dia = -1;
-        		mes = -1;
-        		ano = -1;
-        	}
-        }
-        
-        if ((dia != -1) && (mes != -1) && (ano != -1)) {
-        	this.dataCad = LocalDate.of(ano, mes, dia);
-        }        
-    }
+	        if (ano < 0) {
+	        	dia = -1;
+	            mes = -1;
+	            ano = -1;
+	        }
+	        
+	        if ((mes < 0) && (mes > 13)) {
+	            dia = -1;
+	            mes = -1;
+	            ano = -1;
+	        }
+	        
+	        if (dia < 0) {
+	        	dia = -1;
+	        	mes = -1;
+	        	ano = -1;
+	        }
+	        
+	        if ((mes == 2) && (ano % 4 == 0) && ((ano % 100 != 0) || (ano % 400 == 0))) {
+	            if ((dia < 0) && (dia > 29)) {
+	                dia = -1;
+	                mes = -1;
+	                ano = -1;
+	            }
+	        } else if ((mes == 2) && (dia < 0) && (dia > 28)) {
+	        	dia = -1;
+	            mes = -1;
+	            ano = -1;
+	        }
+	        
+	        if ((mes == 1) || (mes == 3) || (mes == 5) || (mes == 7) || (mes == 8) || (mes == 10) || (mes == 12)) {
+	        	if ((dia < 0) && (dia > 31)) {
+	        		dia = -1;
+	        		mes = -1;
+	        		ano = -1;
+	        	}
+	        }
+	        
+	        if ((mes == 4) || (mes == 6) || (mes == 9) || (mes == 11)) {
+	        	if ((dia < 0) && (dia > 30)) {
+	        		dia = -1;
+	        		mes = -1;
+	        		ano = -1;
+	        	}
+	        }
+	        
+	        if ((dia != -1) && (mes != -1) && (ano != -1)) {
+	        	this.dataCad = LocalDate.of(ano, mes, dia);
+	        }        
+	}
 
 	public String getEmail() {
 		return email;
