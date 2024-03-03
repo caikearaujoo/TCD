010c2f29f70ddb63f61d8fcc18e83aa7acf23e22
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-03-03 17:52:16 -0300

    Update Pessoa.java

--- TCD atualizado/Udi-Decola/Pessoa.java
+++ TCD atualizado/Udi-Decola/Pessoa.java
@@ -1,10 +1,11 @@
 package udidecola;
+
 import validacoes.VerificaCPF;
 import java.time.LocalDate;
 
-// superclasse para cliente e funcionário
+// Pessoa é uma superclasse para Cliente e Funcionário (item 4)
 public class Pessoa {
-  // atributos em comum entre cliente e funcionário
+	// atributos em comum entre Cliente e Funcionário
 	private String nome;
 	private String endLogradouro;
 	private int endNumero;
@@ -15,10 +16,10 @@ public class Pessoa {
 	private String cpf;
 	private LocalDate dataNasc;
 
-  // construtor completo
-	public Pessoa(String nome, String endLogradouro, int endNumero, String endBairro, String endCidade, String endUf, String endCep, 
-			      String cpf, int diaNasc, int mesNasc, int anoNasc) {
-		
+	// construtor (item 3D)
+	public Pessoa(String nome, String endLogradouro, int endNumero, String endBairro, String endCidade, String endUf,
+			String endCep, String cpf, int diaNasc, int mesNasc, int anoNasc) {
+
 		setNome(nome);
 		setEndLogradouro(endLogradouro);
 		setEndNumero(endNumero);
@@ -30,131 +31,153 @@ public class Pessoa {
 		setDataNasc(diaNasc, mesNasc, anoNasc);
 	}
 
-  // construtor padrão
+	// construtor (item 3D)
 	public Pessoa() {
-		
+
 	}
-	
+
+	// getters e setters
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public void setNome(String nome) {
-		this.nome = nome;
+		if (nome.length() > 0)
+			this.nome = nome;
+		else
+			this.nome = null;
 	}
-	
+
 	public String getEndLogradouro() {
 		return endLogradouro;
 	}
-	
+
 	public void setEndLogradouro(String endLogradouro) {
-		this.endLogradouro = endLogradouro;
+		if (endLogradouro.length() > 0)
+			this.endLogradouro = endLogradouro;
+		else
+			this.endLogradouro = null;
 	}
-	
+
 	public int getEndNumero() {
 		return endNumero;
 	}
-	
+
 	public void setEndNumero(int endNumero) {
-		this.endNumero = endNumero;
+		if (endNumero > 0)
+			this.endNumero = endNumero;
+		else
+			this.endNumero = -1;
 	}
-	
+
 	public String getEndBairro() {
 		return endBairro;
 	}
-	
+
 	public void setEndBairro(String endBairro) {
-		this.endBairro = endBairro;
+		if (endBairro.length() > 0)
+			this.endBairro = endBairro;
+		else
+			this.endBairro = null;
 	}
-	
+
 	public String getEndCidade() {
 		return endCidade;
 	}
-	
+
 	public void setEndCidade(String endCidade) {
-		this.endCidade = endCidade;
+		if (endCidade.length() > 0)
+			this.endCidade = endCidade;
+		else
+			this.endCidade = null;
 	}
-	
+
 	public String getEndUf() {
 		return endUf;
 	}
-	
+
 	public void setEndUf(String endUf) {
-		if (endUf.length() == 2) this.endUf = endUf;
-		else endUf = null;
+		if (endUf.length() == 2)
+			this.endUf = endUf;
+		else
+			this.endUf = null;
 	}
-	
+
 	public String getEndCep() {
 		return endCep;
 	}
-	
+
 	public void setEndCep(String endCep) {
-		if (endCep.length() == 8) this.endCep = endCep;
-		else endCep = null;
+		if (endCep.length() == 8)
+			this.endCep = endCep;
+		else
+			this.endCep = null;
 	}
-	
+
 	public String getCpf() {
 		return cpf;
 	}
-	
+
 	public void setCpf(String cpf) {
-		if (VerificaCPF.isCPF(cpf)) this.cpf = cpf;
-		else this.cpf = null;
+		if (VerificaCPF.isCPF(cpf))
+			this.cpf = cpf;
+		else
+			this.cpf = null;
 	}
-	
+
 	public LocalDate getDataNasc() {
 		return dataNasc;
 	}
-	
-    public void setDataNasc(int dia, int mes, int ano) {
-        if (ano < 0) {
-        	dia = -1;
-            mes = -1;
-            ano = -1;
-        }
-        
-        if ((mes < 0) && (mes > 13)) {
-            dia = -1;
-            mes = -1;
-            ano = -1;
-        }
-        
-        if (dia < 0) {
-        	dia = -1;
-        	mes = -1;
-        	ano = -1;
-        }
-        
-        if ((mes == 2) && (ano % 4 == 0) && ((ano % 100 != 0) || (ano % 400 == 0))) {
-            if ((dia < 0) && (dia > 29)) {
-                dia = -1;
-                mes = -1;
-                ano = -1;
-            }
-        } else if ((mes == 2) && (dia < 0) && (dia > 28)) {
-        	dia = -1;
-            mes = -1;
-            ano = -1;
-        }
-        
-        if ((mes == 1) || (mes == 3) || (mes == 5) || (mes == 7) || (mes == 8) || (mes == 10) || (mes == 12)) {
-        	if ((dia < 0) && (dia > 31)) {
-        		dia = -1;
-        		mes = -1;
-        		ano = -1;
-        	}
-        }
-        
-        if ((mes == 4) || (mes == 6) || (mes == 9) || (mes == 11)) {
-        	if ((dia < 0) && (dia > 30)) {
-        		dia = -1;
-        		mes = -1;
-        		ano = -1;
-        	}
-        }
-        
-        if ((dia != -1) && (mes != -1) && (ano != -1)) {
-        	this.dataNasc = LocalDate.of(ano, mes, dia);
-        }        
-    }
+
+	public void setDataNasc(int dia, int mes, int ano) {
+		if (ano < 0) {
+			dia = -1;
+			mes = -1;
+			ano = -1;
+		}
+
+		if ((mes < 0) && (mes > 13)) {
+			dia = -1;
+			mes = -1;
+			ano = -1;
+		}
+
+		if (dia < 0) {
+			dia = -1;
+			mes = -1;
+			ano = -1;
+		}
+
+		if ((mes == 2) && (ano % 4 == 0) && ((ano % 100 != 0) || (ano % 400 == 0))) {
+			if ((dia < 0) && (dia > 29)) {
+				dia = -1;
+				mes = -1;
+				ano = -1;
+			}
+		} else if ((mes == 2) && (dia < 0) && (dia > 28)) {
+			dia = -1;
+			mes = -1;
+			ano = -1;
+		}
+
+		if ((mes == 1) || (mes == 3) || (mes == 5) || (mes == 7) || (mes == 8) || (mes == 10) || (mes == 12)) {
+			if ((dia < 0) && (dia > 31)) {
+				dia = -1;
+				mes = -1;
+				ano = -1;
+			}
+		}
+
+		if ((mes == 4) || (mes == 6) || (mes == 9) || (mes == 11)) {
+			if ((dia < 0) && (dia > 30)) {
+				dia = -1;
+				mes = -1;
+				ano = -1;
+			}
+		}
+
+		if ((dia != -1) && (mes != -1) && (ano != -1)) {
+			this.dataNasc = LocalDate.of(ano, mes, dia);
+		}
+	}
 }
