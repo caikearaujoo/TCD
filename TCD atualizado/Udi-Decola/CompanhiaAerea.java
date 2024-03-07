94890754d9b71fe25e2a74ff2b2fe71da6d27970
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2024-03-07 11:39:22 -0300

    Update CompanhiaAerea.java

--- TCD atualizado/Udi-Decola/CompanhiaAerea.java
+++ TCD atualizado/Udi-Decola/CompanhiaAerea.java
@@ -1,7 +1,8 @@
 
 class CompanhiaAerea extends Empresa{
 	private final static double taxaUdi = 0.05;
-	
+
+	//item 1c) cadastro de companhias aéreas
 	public CompanhiaAerea(String CNPJ, String nome, String nomeDivulg, String endLogradouro, int diaCria, int mesCria,
 			int anoCria, int endNumero, String endBairro, String endCidade, String endUf,
 			String endCep) {
