f88f54b0cab653fb11dd2c34daed6dc0414fddcd
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-03-07 15:58:38 -0300

    Update FuncEspecial.java

--- TCD atualizado/Udi-Decola/FuncEspecial.java
+++ TCD atualizado/Udi-Decola/FuncEspecial.java
@@ -1,27 +1,24 @@
 package udidecola;
 
+// item 4: funcionário especial é subclasse de funcionário do hotel
 public class FuncEspecial extends FuncHotel {
-	// atributos específicos
 	private static double bonusSal;
 	private int hoteisCad;
-	
-	// construtor completo
-	public FuncEspecial(String nome, String endLogradouro, int endNumero, String endBairro, String endCidade, String endUf, String endCep, 
-			            String cpf, int diaNasc, int mesNasc, int anoNasc, 
-			            String carteiraTrab) {
-		
-		super(nome, endLogradouro, endNumero, endBairro, endCidade, endUf, endCep, 
-			  cpf, diaNasc, mesNasc, anoNasc, 
-			  carteiraTrab);
+
+	// item 3D: construtor criado por nós
+	public FuncEspecial(String nome, String endLogradouro, int endNumero, String endBairro, String endCidade,
+			String endUf, String endCep, String cpf, int diaNasc, int mesNasc, int anoNasc, String carteiraTrab) {
+
+		super(nome, endLogradouro, endNumero, endBairro, endCidade, endUf, endCep, cpf, diaNasc, mesNasc, anoNasc,
+				carteiraTrab);
 		setHoteisCad(0);
 	}
-	
-	// construtor com funcionário e data de nascimento
+
+	// item 3D: construtor criado por nós
 	public FuncEspecial(FuncHotel f, int diaNasc, int mesNasc, int anoNasc) {
-		super(f.getNome(), f.getEndLogradouro(), f.getEndNumero(), f.getEndBairro(), f.getEndCidade(), f.getEndUf(), f.getEndCep(), 
-			  f.getCpf(), diaNasc, mesNasc, anoNasc, 
-			  f.getCarteiraTrab());
-		
+		super(f.getNome(), f.getEndLogradouro(), f.getEndNumero(), f.getEndBairro(), f.getEndCidade(), f.getEndUf(),
+				f.getEndCep(), f.getCpf(), diaNasc, mesNasc, anoNasc, f.getCarteiraTrab());
+
 		setHoteisCad(0);
 	}
 
@@ -30,8 +27,10 @@ public class FuncEspecial extends FuncHotel {
 	}
 
 	public static void setBonusSal(double bonusSal) {
-		if (bonusSal > 0) FuncEspecial.bonusSal = bonusSal;
-		else FuncEspecial.bonusSal = -1;
+		if (bonusSal > 0)
+			FuncEspecial.bonusSal = bonusSal;
+		else
+			FuncEspecial.bonusSal = -1;
 	}
 
 	public int getHoteisCad() {
@@ -42,33 +41,28 @@ public class FuncEspecial extends FuncHotel {
 		this.hoteisCad = hoteisCad;
 	}
 
-	// método: definir bônus salarial dos funcionários especiais
+	// método para definir bônus salarial dos funcionários especiais
 	public void definirBonus(double bonus) {
 		setBonusSal(bonus);
 	}
 
-	// método: salário do funcionário especial
+	// item IJ: método para calcular salário do funcionário especial
 	public double calculaSalario(int numHotelRegistrado) {
 		double salario = getSalarioFixo() + (getSalarioFixo() * bonusSal * hoteisCad);
 		return salario;
 	}
-	
-	// método: registro de hoteis
-	public Hotel registraHotel(String CNPJ, String nomeOfi, String nomeDiv, 
-			                   int diaCria, int mesCria, int anoCria, 
-			                   String endLogradouro, int endNumero, String endBairro, String endCidade, String endUf, String endCep, 
-			                   int numEstrelas, boolean petAceito, int numQuartos, String msgDivulg, String descricao,
-			                   int hCheckin,int minCheckin,int segCheckin,
-			      		       int hCheckout,int minCheckout,int segCheckout) {
-		
-		Hotel h = new Hotel(CNPJ, nomeOfi, nomeDiv, 
-				            diaCria, mesCria, anoCria, 
-				            endLogradouro, endNumero, endBairro, endCidade, endUf, endCep, 
-				            numEstrelas, petAceito, numQuartos, msgDivulg, descricao,
-				            hCheckin, minCheckin, segCheckin,
-			    		    hCheckout, minCheckout, segCheckout);
-		
+
+	// método para registro de hoteis
+	public Hotel registraHotel(String CNPJ, String nomeOfi, String nomeDiv, int diaCria, int mesCria, int anoCria,
+			String endLogradouro, int endNumero, String endBairro, String endCidade, String endUf, String endCep,
+			int numEstrelas, boolean petAceito, int numQuartos, String msgDivulg, String descricao, int hCheckin,
+			int minCheckin, int segCheckin, int hCheckout, int minCheckout, int segCheckout) {
+
+		Hotel h = new Hotel(CNPJ, nomeOfi, nomeDiv, diaCria, mesCria, anoCria, endLogradouro, endNumero, endBairro,
+				endCidade, endUf, endCep, numEstrelas, petAceito, numQuartos, msgDivulg, descricao, hCheckin,
+				minCheckin, segCheckin, hCheckout, minCheckout, segCheckout);
+
 		hoteisCad++;
 		return h;
-	}	
+	}
 }
