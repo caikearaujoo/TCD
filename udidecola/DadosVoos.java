42c2ef9a593f1db1fa0f37b154d67f3cc3d82a2d
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2024-03-20 20:37:25 -0300

    Create DadosVoos.java

---
+++ udidecola/DadosVoos.java
@@ -0,0 +1,39 @@
+package tcd;
+
+import java.util.ArrayList;
+
+public class DadosVoos {
+	private ArrayList<Voos> Vooss;
+	
+	public ArrayList<Voos> getVooss() {
+		return Vooss;
+	}
+
+	public boolean cadastrarVoos(Voos Voos) {
+		if (consultarVoos(Voos)) {
+            return false;
+        }
+		else {
+			this.Vooss.add(Voos);
+	        return true;
+		}
+	}
+	
+	public boolean consultarVoos(Voos Voos) {
+	    	
+	    	for(int i = 0; i < Vooss.size(); i++) {
+	    		if(Voos == Vooss.get(i)) return true;
+	    	} 
+	    	return false;	
+	}
+	
+	public boolean removerVoos(Voos Voos) {
+		for(int i = 0; i < Vooss.size(); i++) {
+    		if(Voos == Vooss.get(i)) {
+    			Vooss.remove(i);
+    			return true;
+    		}
+    	}
+		return false;
+	}
+}
