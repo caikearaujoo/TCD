7ba6181ab95dfae5f90d8eaa76e04372c05c593f
Matheus Saliba <127700224+SalibaM@users.noreply.github.com>
2024-03-20 22:12:12 -0300

    Update InterfaceQuartos.java

--- udidecola/InterfaceQuartos.java
+++ udidecola/InterfaceQuartos.java
@@ -26,18 +26,18 @@ public class InterfaceQuartos extends JFrame {
 		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
 		
-		txtMenuDoHotel = new JTextField();
-		txtMenuDoHotel.setHorizontalAlignment(SwingConstants.CENTER);
-		txtMenuDoHotel.setText("Menu do Hotel");
+		txtQuartos = new JTextField();
+		txtQuartos.setHorizontalAlignment(SwingConstants.CENTER);
+		txtQuartos.setText("Menu do Hotel");
 		GridBagConstraints gbc_txtMenuDoHotel = new GridBagConstraints();
-		gbc_txtMenuDoHotel.anchor = GridBagConstraints.NORTH;
-		gbc_txtMenuDoHotel.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtMenuDoHotel.insets = new Insets(0, 0, 5, 0);
-		gbc_txtMenuDoHotel.gridwidth = 4;
-		gbc_txtMenuDoHotel.gridx = 1;
-		gbc_txtMenuDoHotel.gridy = 1;
-		panel.add(txtMenuDoHotel, gbc_txtMenuDoHotel);
-		txtMenuDoHotel.setColumns(10);
+		gbc_txtQuartos.anchor = GridBagConstraints.NORTH;
+		gbc_txtQuartos.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtQuartos.insets = new Insets(0, 0, 5, 0);
+		gbc_txtQuartos.gridwidth = 4;
+		gbc_txtQuartos.gridx = 1;
+		gbc_txtQuartos.gridy = 1;
+		panel.add(txtQuartos, gbc_txtQuartos);
+		txtQuartos.setColumns(10);
 		
 		JButton btnCadastro = new JButton("Cadastrar");
 		GridBagConstraints gbc_btnCadastro = new GridBagConstraints();
