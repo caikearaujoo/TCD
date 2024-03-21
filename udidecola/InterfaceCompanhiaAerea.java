a04ca3b49a31233a1df4fc594b7a650e9cf34d81
Caike Araújo <127609797+caikearaujoo@users.noreply.github.com>
2024-03-20 22:10:57 -0300

    Update InterfaceCompanhiaAerea.java

--- udidecola/InterfaceCompanhiaAerea.java
+++ udidecola/InterfaceCompanhiaAerea.java
@@ -14,7 +14,7 @@ public class InterfaceCompanhiaAerea extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JTextField txtMenuDoHotel;
+	private JTextField txtMenuDoCompanhiaAerea;
 	public InterfaceCompanhiaAerea() {
 		
 		JPanel panel = new JPanel();
@@ -26,18 +26,18 @@ public class InterfaceCompanhiaAerea extends JFrame {
 		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
 		
-		txtMenuDoHotel = new JTextField();
-		txtMenuDoHotel.setHorizontalAlignment(SwingConstants.CENTER);
-		txtMenuDoHotel.setText("Menu do Hotel");
-		GridBagConstraints gbc_txtMenuDoHotel = new GridBagConstraints();
-		gbc_txtMenuDoHotel.anchor = GridBagConstraints.NORTH;
-		gbc_txtMenuDoHotel.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtMenuDoHotel.insets = new Insets(0, 0, 5, 0);
-		gbc_txtMenuDoHotel.gridwidth = 4;
-		gbc_txtMenuDoHotel.gridx = 1;
-		gbc_txtMenuDoHotel.gridy = 1;
-		panel.add(txtMenuDoHotel, gbc_txtMenuDoHotel);
-		txtMenuDoHotel.setColumns(10);
+		txtMenuDoCompanhiaAerea = new JTextField();
+		txtMenuDoCompanhiaAerea.setHorizontalAlignment(SwingConstants.CENTER);
+		txtMenuDoCompanhiaAerea.setText("Menu do Companhia Aerea");
+		GridBagConstraints gbc_txtMenuDoCompanhiaAerea = new GridBagConstraints();
+		gbc_txtMenuDoCompanhiaAerea.anchor = GridBagConstraints.NORTH;
+		gbc_txtMenuDoCompanhiaAerea.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMenuDoCompanhiaAerea.insets = new Insets(0, 0, 5, 0);
+		gbc_txtMenuDoCompanhiaAerea.gridwidth = 4;
+		gbc_txtMenuDoCompanhiaAerea.gridx = 1;
+		gbc_txtMenuDoCompanhiaAerea.gridy = 1;
+		panel.add(txtMenuDoCompanhiaAerea, gbc_txtMenuDoCompanhiaAerea);
+		txtMenuDoCompanhiaAerea.setColumns(10);
 		
 		JButton btnCadastro = new JButton("Cadastrar");
 		GridBagConstraints gbc_btnCadastro = new GridBagConstraints();
