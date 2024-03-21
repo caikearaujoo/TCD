ce68344997feabd855f6afe676c84e267c5b5fc6
amanda duarte <127355878+aduarte09@users.noreply.github.com>
2024-03-21 10:17:54 -0300

    Update DadosCompanhiaAerea.java

--- udidecola/DadosCompanhiaAerea.java
+++ udidecola/DadosCompanhiaAerea.java
@@ -3,37 +3,39 @@ package tcd;
 import java.util.ArrayList;
 
 public class DadosCompanhiaAerea {
-private ArrayList<CompanhiaAerea> CompanhiasAereas;
-	
+	private ArrayList<CompanhiaAerea> CompanhiasAereas;
+
 	public ArrayList<CompanhiaAerea> getCompanhiasAereas() {
 		return CompanhiasAereas;
 	}
 
 	public boolean cadastrarCompanhiaAerea(CompanhiaAerea CompanhiaAerea) {
-		if (consultarCompanhiaAerea(CompanhiaAerea)) {
-            return false;
-        }
-		else {
+		if (consultarCompanhiaAerea(CompanhiaAerea.getCNPJ())) {
+			return false;
+		} else {
 			this.CompanhiasAereas.add(CompanhiaAerea);
-	        return true;
+			return true;
 		}
 	}
-	
-	public boolean consultarCompanhiaAerea(CompanhiaAerea CompanhiaAerea) {
-	    	
-	    	for(int i = 0; i < CompanhiasAereas.size(); i++) {
-	    		if(CompanhiaAerea == CompanhiasAereas.get(i)) return true;
-	    	} 
-	    	return false;	
+
+	public boolean consultarCompanhiaAerea(String cnpj) {
+		for (int i = 0; i < CompanhiasAereas.size(); i++) {
+			CompanhiaAerea temp = CompanhiasAereas.get(i);
+			if (cnpj == temp.getCNPJ()) {
+				return true;
+			}
+		}
+		return false;
 	}
-	
-	public boolean removerCompanhiaAerea(CompanhiaAerea CompanhiaAerea) {
-		for(int i = 0; i < CompanhiasAereas.size(); i++) {
-    		if(CompanhiaAerea == CompanhiasAereas.get(i)) {
-    			CompanhiasAereas.remove(i);
-    			return true;
-    		}
-    	}
+
+	public boolean removerCompanhiaAerea(String cnpj) {
+		for (int i = 0; i < CompanhiasAereas.size(); i++) {
+			CompanhiaAerea temp = CompanhiasAereas.get(i);
+			if (cnpj == temp.getCNPJ()) {
+				CompanhiasAereas.remove(i);
+				return true;
+			}
+		}
 		return false;
 	}
 }
